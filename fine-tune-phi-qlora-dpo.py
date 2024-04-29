d63a76a0fbd85bb805def71cd21701a2b96f6fc2
Brian96086 <brian96086@gmail.com>
2024-04-29 13:53:38 -0400

    added scripts that run on greatlakes

--- fine-tune-phi-qlora-dpo.py
+++ fine-tune-phi-qlora-dpo.py
@@ -16,6 +16,7 @@ import argparse
 import tqdm
 import gc
 import torch
+import os
 
 
 PHI_2_MODEL_ID = 'microsoft/phi-2'
@@ -29,6 +30,9 @@ parser.add_argument('--dataset',
 parser.add_argument('--model_name',
                     type=str,
                     help='Name of the model.')
+parser.add_argument('--save_root',
+                    type=str,
+                    help='Root directory to store saved results.')
 parser.add_argument('--is_online',
                     action='store_true',
                     help='Indicates whether or not to use online distillation or not.')
@@ -40,6 +44,10 @@ parser.add_argument('--num_epochs',
                     type=int,
                     default=3,
                     help='Number of epochs to train for.')
+parser.add_argument('--curr_epoch',
+                    type=int,
+                    default=0,
+                    help='Current epoch for online distillation.')
 
 # Parse command line arguments
 args = parser.parse_args()
@@ -48,19 +56,31 @@ model_name = args.model_name
 is_online = args.is_online
 lr = args.lr
 num_epochs = args.num_epochs
+curr_epoch = args.curr_epoch
+print(f'is_online = {is_online}')
 
 
+#online_status = "online" if (is_online or curr_epoch !=0) else "offline"
+online_status = "online" if num_epochs == 1 else "offline"
+
+root_dir = args.save_root
+input_dir = f'{root_dir}/code_{dataset}/{online_status}_lr={lr}_e={curr_epoch-1}'
+output_dir = f'{root_dir}/code_{dataset}/{online_status}_lr={lr}_e={curr_epoch}'
+os.makedirs(output_dir, exist_ok = True)
+if(not os.path.exists(input_dir)): input_dir = PHI_2_MODEL_ID
 
 # Load the Phi-2 tokenizer and define how the tokens should be padded
-tokenizer = AutoTokenizer.from_pretrained(PHI_2_MODEL_ID,
-                                          add_eos_token=True,
-                                          use_fast=True)
+# tokenizer = AutoTokenizer.from_pretrained(PHI_2_MODEL_ID,
+#                                           add_eos_token=True,
+#                                           use_fast=True)
+tokenizer = AutoTokenizer.from_pretrained(input_dir, add_eos_token=True,  use_fast=True)
 tokenizer.padding_side = 'right'
 tokenizer.pad_token = tokenizer.eos_token
 
-
 # Json data is in an incorrect form -- need to switch "rows" and "columns"
-if dataset == 'summarization':
+if(curr_epoch!=0):
+    json_file = open(f'{input_dir}/result.json')
+elif dataset == 'summarization':
     json_file = open('./datasets/code_summary_dataset.json')
 elif dataset == 'generation':
     json_file = open('./datasets/code_solution_dataset.json')
@@ -69,12 +89,14 @@ else:
 
 json_data = json.load(json_file)
 data_list = []
+rejected_key = 'rejected' if curr_epoch ==0 else 'trained_output'
 for i in range(len(json_data['prompt'])):
     sample = {}
     sample['prompt'] = json_data['prompt'][i]
     sample['chosen'] = json_data['chosen'][i]
-    sample['rejected'] = json_data['rejected'][i]
-    data_list.append(sample)
+    sample['rejected'] = json_data[rejected_key][i]
+    if(sample['rejected']!=None): # 3 examples encountered error during data generation
+        data_list.append(sample)
 json_file.close()
 
 # Load the transformed dataset
@@ -88,13 +110,13 @@ bnb_config = BitsAndBytesConfig(
         bnb_4bit_use_double_quant=True,
         load_in_4bit=True
 )
-model = AutoModelForCausalLM.from_pretrained(
-          PHI_2_MODEL_ID, 
-          quantization_config=bnb_config,
-          trust_remote_code=True
-)
+# model = AutoModelForCausalLM.from_pretrained(
+#           PHI_2_MODEL_ID, 
+#           quantization_config=bnb_config,
+#           trust_remote_code=True
+# )
+model = AutoModelForCausalLM.from_pretrained(input_dir, quantization_config=bnb_config, trust_remote_code=True)
 model = prepare_model_for_kbit_training(model)
-
 
 # Define the QLORA settings
 peft_config = LoraConfig(
@@ -117,7 +139,8 @@ if not is_online:
             optim='paged_adamw_8bit',
             bf16=True, #change to fp16 if are using an older GPU
             num_train_epochs=num_epochs,
-            report_to=None)
+            report_to=None,
+            logging_steps = 50)
 
 
     # Create the DPO Trainer
@@ -135,80 +158,6 @@ if not is_online:
     dpo_trainer.train()
 
     # Save the fine-tuned model
-    dpo_trainer.save_model('./trained_models/' + model_name)
-
-
-elif is_online:
-
-    for epoch in range(num_epochs):
-
-        # If not the first epoch, generate a new dataset with the latest fine-tuned Phi-2 model
-        if epoch != 0:
-            
-            # Load the latest tokenizer/model
-            tokenizer = AutoTokenizer.from_pretrained('./trained_models/' + model_name + '_' + str(epoch-1))
-            model = AutoModelForCausalLM.from_pretrained('./trained_models/' + model_name + '_' + str(epoch-1), quantization_config=bnb_config)
-
-            # Load original dataset
-            if dataset == 'summarization':
-                json_file = open('./datasets/code_summary_dataset.json')
-            elif dataset == 'generation':
-                json_file = open('./datasets/code_solution_dataset.json')
-            else:
-                raise ValueError('--dataset flag must be \"summarization\" or \"generation\"')
-
-            # Create new dataset for current epoch using latest model
-            json_data = json.load(json_file)
-            data_list = []
-            print("Generating training set for epoch " + str(epoch))
-            for i in tqdm.tqdm(range(len(json_data['prompt'])), position=0, leave=True):
-                sample = {}
-                sample['prompt'] = json_data['prompt'][i]
-                sample['chosen'] = json_data['chosen'][i]
-                inputs = tokenizer(sample['prompt'], return_tensors="pt", return_attention_mask=False).to('cuda')
-                outputs = model.generate(**inputs, max_length=500, pad_token_id=tokenizer.eos_token_id)
-                rejected = tokenizer.batch_decode(outputs)[0]
-                sample['rejected'] = rejected
-                data_list.append(sample)
-            json_file.close()
-
-            # Load the transformed dataset
-            phi_dataset = Dataset.from_list(data_list)
-
-
-        # Define the training arguments
-        training_arguments = TrainingArguments(
-            output_dir='./phi-2-training-results',
-            per_device_train_batch_size=1,
-            learning_rate=lr,
-            optim='paged_adamw_8bit',
-            bf16=True, #change to fp16 if are using an older GPU
-            num_train_epochs=1,
-            report_to=None
-        )
-
-        # Create the DPO Trainer
-        dpo_trainer = DPOTrainer(
-            model=model,
-            train_dataset=phi_dataset,
-            peft_config=peft_config,
-            max_prompt_length=512,
-            max_length=1024,
-            tokenizer=tokenizer,
-            args=training_arguments,
-        )
-
-        # Start the process of fine-tuning Phi-2 with DPO
-        dpo_trainer.train()
-
-        # Save the fine-tuned model
-        dpo_trainer.save_model('./trained_models/' + model_name + '_' + str(epoch))
-
-        # Free memory
-        torch._C._cuda_clearCublasWorkspaces()
-        torch._dynamo.reset()
-        del tokenizer
-        del model
-        del dpo_trainer
-        gc.collect()
-        torch.cuda.empty_cache() 
+    dpo_trainer.save_model(output_dir)
+else:
+    raise Exception('online training can only be done by calling offline mode multiple times')
--- phi2_query.py
+++ phi2_query.py
@@ -1,9 +1,13 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, AutoModel,PhiForCausalLM
 import torch
-
+import json
+from tqdm import tqdm
+import os
+import argparse
 
 from IPython.display import display
 from IPython.display import Markdown
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
 
 def to_markdown(text):
@@ -14,14 +18,75 @@ def to_markdown(text):
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_dir',
+                    type=str,
+                    help='dir to trained model')
+parser.add_argument('--dataset', type = str)
+parser.add_argument('--trials', type = int, default = 3)
+parser.add_argument('--trial_num', type = int, default = 0)
+args = parser.parse_args()
+input_dir = args.input_dir
+dataset = args.dataset
+trials = args.trials
+trial_num = args.trial_num
+
+
 torch.set_default_device("cuda")
-model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
-tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
+#model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
 
-prompt = "How are you?"
-inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False).to('cuda')
+tokenizer = AutoTokenizer.from_pretrained(input_dir, trust_remote_code=True, use_fast=True)
+#model = AutoModel.from_pretrained('/home/bswang/Courses/EECS545/eecs-545-project/phi-2-training-results/checkpoint-7000')
+model = PhiForCausalLM.from_pretrained(input_dir, token = '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').to('cuda')
 
-outputs = model.generate(**inputs, max_length=500, pad_token_id=tokenizer.eos_token_id)
+if(os.path.exists(os.path.join(input_dir, f'result.json'))):
+  json_file = open(os.path.join(input_dir, f'result.json'))
+elif(dataset == "generation"):
+  json_file = open('./datasets/code_solution_dataset.json')
+elif(dataset == "summarization"):
+  json_file = open('./datasets/code_summary_dataset.json')
+else:
+  raise Exception('please double check your dataset input')
+json_data = json.load(json_file)
+datalist = []
 
-text = tokenizer.batch_decode(outputs)[0]
-to_markdown(text)
+for i in range(len(json_data['prompt'])):
+  datalist.append(json_data['prompt'][i])
+
+start_idx = 0
+output_text = [] if 'trained_output' not in json_data else json_data['trained_output']
+if(len(output_text) == 0 or len(output_text) % (len(datalist) // trials) == 0):
+  ratio = len(output_text) / len(datalist)
+  ratio += 1/trials * trial_num
+  start_idx, end_idx = int(ratio * len(datalist)), int((ratio + 1/trials) * len(datalist))
+  end_idx = min(end_idx, len(datalist))
+else:
+  raise Exception('Please check difference that causes incorrect size')
+
+print(f'start = {start_idx}, end = {end_idx}')
+if(start_idx == len(datalist)): exit()
+for i in tqdm(range(start_idx, end_idx)):
+  try:
+    prompt = datalist[i]
+    inputs = tokenizer(prompt, return_tensors="pt", return_attention_mask=False).to('cuda')
+    
+    if(args.dataset == "generation"):
+      outputs = model.generate(**inputs, max_new_tokens=500, pad_token_id=tokenizer.eos_token_id)
+    else: #summarization
+      outputs = model.generate(**inputs, max_length=len(prompt)+300, pad_token_id=tokenizer.eos_token_id)
+    text = tokenizer.batch_decode(outputs)[0]
+    output_text.append(text)
+    
+  except: #if error arises (3 samples in dataset)
+    output_text.append(None)
+
+  if(i == (start_idx + end_idx) // 2): #save halfway in case the process was way slower than expected
+    json_data['trained_output'] = output_text
+    with open(os.path.join(input_dir,f'result_{start_idx}.json'), 'w') as f:
+      json.dump(json_data, f)
+
+print('answer generated')
+json_data['trained_output'] = output_text
+with open(os.path.join(input_dir,f'result_{start_idx}.json'), 'w') as f:
+    json.dump(json_data, f)
+#to_markdown(text)
